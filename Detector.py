0cdf80639e9311f1e4d3628a4548455c798ae8b6
yuni <qurrata111@gmail.com>
2021-10-08 10:22:30 +0700

    add backward pooling & detection

--- Detector.py
+++ Detector.py
@@ -1,34 +1,45 @@
-import math
 import numpy as np
 
-# Input berupa matriks setelah convolution 
-# Output berupa matriks yang telah diaktivasi
 class Detector:
-    def __init__(self):
-        self.input = [] 
-        self.activation_type = 'sigmoid'
+    def __init__(self, matrix, activation_type):
+        self.input = np.array(matrix)
+        self.activation_type = activation_type
 
-    def activate(self, rearrange_output, activation_type='sigmoid'):
-        self.input = rearrange_output
+    def forward(self):
         result = []
-        for rearrange in rearrange_output:
-            if activation_type == 'sigmoid':
-                result.append(list(self.sigmoid(rearrange)))
-            elif activation_type == 'relu':
-                result.append(list(self.relu(rearrange)))
-            elif activation_type == 'softmax':
-                result.append(list(self.softmax(rearrange)))
+        for rearrange in self.input:
+            if self.activation_type == 'sigmoid':
+                result.append(list(sigmoid(rearrange)))
+            elif self.activation_type == 'relu':
+                result.append(list(relu(rearrange)))
+            elif self.activation_type == 'softmax':
+                result.append(list(softmax(rearrange)))
+            else:
+                result.append(list(rearrange))
+        return result
+
+    def backward(self):
+        result = []
+        for rearrange in self.input:
+            if self.activation_type == 'sigmoid':
+                result.append(list(dSigmoid(rearrange)))
+            elif self.activation_type == 'relu':
+                result.append(list(dRelu(rearrange)))
             else:
                 result.append(list(rearrange))
         return result
     
-    def sigmoid(self, X):
-        return 1/(1+np.exp(-X))
+def sigmoid(X):
+    return 1/(1+np.exp(-X))
 
-    def relu(self, X):
-        return np.maximum(0,X)
-    
-    def softmax(self, X):
-        expo = np.exp(X)
-        expo_sum = np.sum(np.exp(X))
-        return expo/expo_sum
+def dSigmoid(X):
+    return sigmoid(X)*(1-sigmoid(X))
+
+def relu(X):
+    return np.maximum(0,X)
+
+def dRelu(X):
+    return np.where(X < 0, 0, 1)
+
+def softmax(X):
+    return np.exp(X) / np.sum(np.exp(X))
--- Pooling.py
+++ Pooling.py
@@ -3,56 +3,108 @@ from numpy.lib.stride_tricks import as_strided
 from Detector import Detector
 
 class Pooling:
-    def __init__(self):
-        self.matrix = []
-        self.kernel_size = 0
-        self.stride= 0
-        self.padding = 0
-        self.pool_mode = 'max'
-
-    def apply(self, matrix, kernel_size=2, stride=2, padding=0, pool_mode='max'):
-        self.matrix = matrix
+    def __init__(self, matrix, kernel_size, stride=2, padding=0, pool_mode='max'):
+        self.input = matrix
         self.kernel_size = kernel_size
         self.stride= stride
         self.padding = padding
         self.pool_mode = pool_mode
 
-        self.matrix = np.pad(self.matrix, self.padding, mode='constant')
+        self.output_shape = (0, 0)
+        self.input_reshape = []
+
+        self.forward_output = []
+        self.backward_output = []
+
+    def forward(self):
+        self.input = np.pad(self.input, self.padding, mode='constant')
 
         output_shape = (
-            (self.matrix.shape[0] - self.kernel_size)//self.stride + 1,
-            (self.matrix.shape[1] - self.kernel_size)//self.stride + 1
+            (self.input.shape[0] - self.kernel_size[0] + 2*self.padding)//self.stride + 1,
+            (self.input.shape[1] - self.kernel_size[1] + 2*self.padding)//self.stride + 1
         )
-
-        self.kernel_size = (self.kernel_size, self.kernel_size)
+        self.output_shape = output_shape
 
         matrix_w = as_strided(
-            self.matrix,
+            self.input,
             shape = output_shape + self.kernel_size,
-            strides = (self.stride*self.matrix.strides[0], self.stride*self.matrix.strides[1]) + self.matrix.strides
+            strides = (self.stride*self.input.strides[0], self.stride*self.input.strides[1]) + self.input.strides
         )
         matrix_w = matrix_w.reshape(-1, *self.kernel_size)
 
-        if self.pool_mode == 'max':
-            return matrix_w.max(axis=(1,2)).reshape(output_shape)
-        elif self.pool_mode == 'avg':
-            return matrix_w.mean(axis=(1,2)).reshape(output_shape)
+        self.input_reshape = matrix_w
 
-    def infoLayer(self):
-        print(self.filter_size[0], self.filter_size[1])
-        print(self.stride_size)
-        print(self.mode)
-    
-matrix = np.array([
+        if self.pool_mode == 'max':
+            self.forward_output = matrix_w.max(axis=(1,2)).reshape(output_shape)
+        elif self.pool_mode == 'avg':
+            self.forward_output = matrix_w.mean(axis=(1,2)).reshape(output_shape)
+        return self.forward_output
+
+    def backward(self):
+        reshape_forward_output = self.forward_output.flatten()
+        mask = []
+        for i in range (0, len(self.input_reshape)):
+            mask.append(self.input_reshape[i] == reshape_forward_output[i])
+        mask = np.array(mask)
+        
+        output = []
+        if self.pool_mode == 'max':
+            for i in range (len(mask)):
+                temp_output = []
+                for j in range (len(mask[i])):
+                    temp_row = []
+                    for k in range (len(mask[i][j])):
+                        temp_row.append(self.input_reshape[i][j][k]) if mask[i][j][k] else temp_row.append(-999)
+                    temp_output.append(temp_row)
+                output.append(temp_output)
+        elif self.pool_mode == 'avg':
+            for i in range (len(self.input_reshape)):
+                temp_output = []
+                for j in range (len(self.input_reshape[i])):
+                    temp_row = []
+                    for k in range (len(self.input_reshape[i][j])):
+                        temp_row.append(reshape_forward_output[i] / len(self.input_reshape))
+                    temp_output.append(temp_row)
+                output.append(temp_output)
+        self.backward_output = output
+        return self.backward_output
+
+matrix = [
     [1, 1, 2, 4],
     [5, 6, 7, 8],
     [3, 2, 1, 0],
     [1, 2, -3, 4]
-])
+]
 
-detector = Detector()
-output_detector = detector.activate(matrix, '')
-print("output detector:", output_detector)
-pooling = Pooling()
-output_pooling = pooling.apply(output_detector, kernel_size=2, stride=2, padding=0, pool_mode='max')
-print("output_pooling:", output_pooling)
+matrices =[
+    [
+        [-85, 76, 64],
+        [109, -1, 10],
+        [118, 71, 67]
+    ],
+    [
+        [-144, -291, 66],
+        [-347, 102, -192],
+        [-239, -52, -162]
+    ]
+]
+
+# FORWARD
+for matrix in matrices:
+    detector = Detector(matrix, 'relu')
+    output_detector = detector.forward()
+    print(detector.input)
+    print("FORWARD DETECTOR :", np.array(output_detector))
+    # pooling = Pooling(output_detector, kernel_size=(2,2), stride=2, padding=0, pool_mode='max')
+    pooling = Pooling(output_detector, kernel_size=(3,3), stride=1, padding=0, pool_mode='max')
+    forward_pooling = pooling.forward()
+    print("FORWARD POOL :", np.array(forward_pooling))
+
+    # BACKWARD
+    backward_pooling = pooling.backward()
+    backward_pooling = np.array(backward_pooling).reshape(pooling.input.shape)
+    print("BACKWARD POOL:", backward_pooling)
+    dDetector = Detector(backward_pooling, 'relu')
+    print("BACKWARD POOL ACTIVATION:", np.array(dDetector.backward()))
+    print("BACKWARD DETECTOR:", np.array(detector.backward()))
+    print()
