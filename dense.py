c958ffe31066cab6ea870fe1e3baec890dd653c4
dikaa <13518007@std.stei.itb.ac.id>
2021-10-08 17:19:35 +0700

    add backprop dense

--- dense.py
+++ dense.py
@@ -32,13 +32,52 @@ class Dense:
         result = []
         for i in dot:
             result.append(self.activation(i))
+        self.output = result
         return result
 
-# TEST
-# dense = Dense(2, 'sigmoid')
-# dense.build(3, (-3,3))
-# print(dense.weight)
-# dot = dense.compute_dot([[3,2,1],[1,2,3]])
-# print('DOT : ', dot)
-# output = dense.compute_output(dot)
-# print('OUTPUT : ', output)
+    def predict(self, input_matriks):
+        dot = self.compute_dot(input_matriks)
+        output = self.compute_output(dot)
+        return output
+
+    def train(self, inputs, target, epoch, l_rate):
+        for iteration in range(epoch):
+            # Pass the training set through the network.
+            output = self.predict(inputs)
+            # Calculate the error
+            error = []
+            for batch_idx, batch in enumerate(output):
+                batch_error = []
+                for value_idx, x in enumerate(batch):
+                    batch_error.append(((target[batch_idx][value_idx] - x)**2)/2)
+                error.append(sum(batch_error))
+                        
+            # Update weight
+            for i, batch in enumerate(self.output):
+                for j, x in enumerate(batch):
+                    t = target[i][j]
+                    y = self.output[i][j]
+                    delta = self.sigmoid_derivative(y) * (t - y) * -1 * y
+                    new_weight = []
+                    y = self.weight
+                    for weights in y:
+                        x = weights
+                        w = x[j] - delta*l_rate
+                        x[j] = w
+                        new_weight.append(x)
+                    self.weight = new_weight
+            return error
+
+    def sigmoid_derivative(self, x):
+        return x * (1 - x)
+    
+
+#TEST
+inputs = [[3,2,1]]
+target = [[0,1]]
+dense = Dense(2, 'sigmoid')
+dense.build(3, (-3,3))
+error = dense.train(inputs, target, 30, 0.5)
+output = dense.predict([[3,2,1]])
+print(output)
+#dense.train([[1, 0], [0, 1]], 10)
